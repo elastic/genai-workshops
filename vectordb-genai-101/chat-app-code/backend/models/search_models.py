4fa578cbe188502afdd6ca23e5b33a3d6c355bb7
derickson <david.erickson@gmail.com>
2024-10-08 18:09:47 -0400

    make it so

--- vectordb-genai-101/chat-app-code/backend/models/search_models.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class SearchQuery(BaseModel):
-    query: str
-    context_type: str  # This matches the payload from the frontend correctly now
--- vectordb-genai-101/chat-app-code/frontend/src/components/index.tsx
+++
@@ -1,3 +0,0 @@
-// src/components/index.tsx
-// export { main_chat as MainChat } from './main_chat';
-export { default as MainChat } from './main_chat';
