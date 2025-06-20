2be7798f788fcf9f6dfc6e45ef762a9b704fe0d6
liladler <50909742+liladler@users.noreply.github.com>
2025-06-20 17:50:51 +0100

    Update elastic.py

--- elasticlm-main/backend/mcp_servers/elastic.py
+++ elasticlm-main/backend/mcp_servers/elastic.py
@@ -6,9 +6,11 @@ from . import register
 
 params = StdioServerParameters(
     command="npx",
-    args=["-y", "@elastic/mcp-server-elasticsearch"],
+    args=["-y", "@elastic/mcp-server-elasticsearch@0.2.0"],
     env={"ES_URL": settings.ES_URL,
-         "ES_API_KEY": settings.ES_API_KEY},
+         "ES_API_KEY": settings.ES_API_KEY,
+         "ES_VERSION": "8"
+        },
 )
 
 elastic_mcp = MCPServerProcess(params, tag="elastic-mcp")
